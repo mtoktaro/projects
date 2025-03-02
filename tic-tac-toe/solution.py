c3bc8026579ee90498dcd7253b5831218abfe079
c-max.toktarov <c-max.toktarov@charter.com>
2025-03-02 14:07:26 -0700

    draw

--- tic-tac-toe/solution.py
+++ tic-tac-toe/solution.py
@@ -3,21 +3,30 @@ def main():
     move=False
     
     matrix=[['.']*3 for i in range(3)]
-
+    count=0
     while move==False:
         p1=input("enter your coordinate: ")
         p1=p1.split(' ')
         matrix[int(p1[0])][int(p1[1])]='X'
+        count+=1
         
         for i in matrix:
             print(i)
+            
+        if count==9:
+            print('Draw')
+            return 0
+
+        
 
         p2=input("enter your coordinate: ")
         p2=p2.split(' ')
         matrix[int(p2[0])][int(p2[1])]='O'
+        count+=1
 
         for i in matrix:
             print(i)
+        
         
         return 0    
 
