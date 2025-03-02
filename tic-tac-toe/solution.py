8a18ed0d1d8e15bf3cf5ddddbe5c91789bd47899
c-max.toktarov <c-max.toktarov@charter.com>
2025-03-01 23:12:20 -0700

    first commit

---
+++ tic-tac-toe/solution.py
@@ -0,0 +1,26 @@
+def main():
+    print('hello')
+    move=False
+    
+    matrix=[['.']*3 for i in range(3)]
+
+    while move==False:
+        p1=input("enter your coordinate: ")
+        p1=p1.split(' ')
+        matrix[int(p1[0])][int(p1[1])]='X'
+        
+        for i in matrix:
+            print(i)
+
+        p2=input("enter your coordinate: ")
+        p2=p2.split(' ')
+        matrix[int(p2[0])][int(p2[1])]='O'
+
+        for i in matrix:
+            print(i)
+        return 0
+
+
+
+if __name__ == '__main__':
+    main()
