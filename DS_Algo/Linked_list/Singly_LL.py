10349ba8ad69ce0a04500001cd864dca471ec853
c-max.toktarov <c-max.toktarov@charter.com>
2025-03-12 22:00:02 -0600

    singly ll

---
+++ DS_Algo/Linked_list/Singly_LL.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, v):
+        self.value=v
+        self.next=None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+
+
+
+def main():
+    
+
+
+    
+
+    
+
+
+    k=LL(3)
+    print(k)
+
+if __name__ == '__main__':  
+    main()
