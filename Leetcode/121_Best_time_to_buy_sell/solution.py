6231866dc6860e0dec6566dee83613361b430435
c-max.toktarov <c-max.toktarov@charter.com>
2025-04-12 23:47:27 -0600

    leetcode

---
+++ Leetcode/121_Best_time_to_buy_sell/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        n = len(prices)
+        ans = 0
+        max_p = 0
+        for i in range(n-1,-1, -1):
+            ans = max(max_p - prices[i], ans)
+            max_p = max(max_p, prices[i])
+
+   
+        
+        return ans
