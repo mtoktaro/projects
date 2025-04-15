e042c0896529be7877e5039055e7251bd548d6c3
c-max.toktarov <c-max.toktarov@charter.com>
2025-04-15 10:28:48 -0600

    syntax

--- Leetcode/79_Word_Search/solution.py
+++ Leetcode/79_Word_Search/solution.py
@@ -10,33 +10,31 @@ class Solution:
         n = len(board)
         m = len(board[0])
 
-
-
-        def dfs(board, row, column, s_pos, visited):
-            if s_pos == (len(word) - 1):
-                return True
-            
-            visited.add((row, column))
-            
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-            for dx, dy in directions:
-
-                if (row + dx >= 0) and (row + dx < n) and (column + dy >= 0) and (column + dy < m) and (board[row + dx][column+ dy]) == word[s_pos + 1] and (row + dx, column + dy) not in visited:
-                    if dfs(board, row + dx, column + dy, s_pos + 1, visited):
-                        return True
-
-            
-            visited.remove((row, column))
-            return False
-
         for row in range(len(board)):
             for column in range(len(board[0])):
                 if board[row][column] == word[0]:
-                    print(row, column)
-                    if dfs(board, row, column, 0, set()):
+                    
+                    if self.dfs(board, row, column, n, m, 0, set()):
                         return True
 
+        return False
+
+    def dfs(self, board, row, column, n, m, s_pos, visited):
+        if s_pos == (len(word) - 1):
+            return True
+        
+        visited.add((row, column))
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        for dx, dy in directions:
+            if (row + dx >= 0) and (row + dx < n) and (column + dy >= 0) and (column + dy < m) and \
+               (board[row + dx][column + dy] == word[s_pos + 1]) and ((row + dx, column + dy) not in visited):
+        
+                if self.dfs(board, row + dx, column + dy, n, m, s_pos + 1, visited):
+                    return True
+
+        
+        visited.remove((row, column))
         return False
         
 
