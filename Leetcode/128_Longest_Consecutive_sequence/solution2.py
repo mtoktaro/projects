2849dd52807f6dec15026c6de69418b069a10e2e
c-max.toktarov <c-max.toktarov@charter.com>
2025-04-24 20:00:11 -0600

    leetcode

---
+++ Leetcode/128_Longest_Consecutive_sequence/solution2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        sett = set()
+        ans = 0
+        l = []
+        for i in nums:
+            sett.add(i)
+
+        for i in sett:
+            if i-1 not in sett:
+                l.append(i)
+
+        for i in l:
+            k = i
+            length = 0
+            while k in sett:
+                length += 1
+                k += 1  
+            ans = max(ans, length)
+        return ans
+    
+l = [100,4,200,1,3,2]
+
+print(Solution().longestConsecutive(l))
